6796cc2f188b0254f64ba6d7fbf9e9023122e36b
unknown <nathan-lee1111@outlook.com>
2023-02-08 13:56:29 -0600

    fix script.ts (+ amogus for ian)

--- build/script.js
+++ build/script.js
@@ -1,70 +1,14 @@
 "use strict";
+/* eslint-disable no-unused-vars */
 function compactNav() {
     var element = document.getElementById("topNav");
-    if (element.className === "nav") {
+    if (element === null)
+        return;
+    if (element.className === "homeNav") {
         element.className += " responsive";
     }
     else {
-        element.className = "nav";
+        element.className = "homeNav";
     }
 }
-
-function compactNav2() {
-    var element = document.getElementById("topNav2");
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
-}
-
-function compactNav3() {
-    var element = document.getElementById("topNav3");
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
-}
-
-function compactNav4() {
-    var element = document.getElementById("topNav4");
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
-}
-
-function compactNav5() {
-    var element = document.getElementById("topNav5");
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
-}
-
-function compactNav6() {
-    var element = document.getElementById("topNav6");
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
-}
-
-function compactNav7() {
-    var element = document.getElementById("topNav7");
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
-}
+/* eslint-enable no-unused-vars */ 
--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,11 @@
+/* eslint-disable no-unused-vars */
 function compactNav() {
-    let element = document.getElementById("topNav") as HTMLElement;
+    let element = document.getElementById("topNav");
+    if (element === null) return;
     if (element.className === "homeNav") {
         element.className += " responsive";
     } else {
         element.className = "homeNav";
     }
 }
+/* eslint-enable no-unused-vars */
