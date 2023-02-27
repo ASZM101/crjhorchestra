62963429fe4cfb47c082683fc1adbd0d9cb71bda
unknown <nathan-lee1111@outlook.com>
2023-02-27 14:08:37 -0600

    zycie jest bez sensu i wszyscy zginiemy

--- build/script.js
+++ build/script.js
@@ -1,67 +1,40 @@
 "use strict";
 /* eslint-disable @typescript-eslint/no-unused-vars */
-function compactNav() {
-    var element = document.getElementById("topNav");
+function compactNavLogic(element) {
     if (element.className === "nav") {
         element.className += " responsive";
     }
     else {
         element.className = "nav";
     }
+}
+function compactNav() {
+    var element = document.getElementById("topNav");
+    compactNavLogic(element);
 }
 function compactNav2() {
     var element = document.getElementById("topNav2");
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
+    compactNavLogic(element);
 }
 function compactNav3() {
     var element = document.getElementById("topNav3");
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
+    compactNavLogic(element);
 }
 function compactNav4() {
     var element = document.getElementById("topNav4");
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
+    compactNavLogic(element);
 }
 function compactNav5() {
     var element = document.getElementById("topNav5");
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
+    compactNavLogic(element);
 }
 function compactNav6() {
     var element = document.getElementById("topNav6");
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
+    compactNavLogic(element);
 }
 function compactNav7() {
     var element = document.getElementById("topNav7");
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
+    compactNavLogic(element);
 }
 /* eslint-enable @typescript-eslint/no-unused-vars
  */ 
--- src/script.ts
+++ src/script.ts
@@ -1,72 +1,45 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-function compactNav() {
-    const element = document.getElementById("topNav") as HTMLElement;
+function compactNavLogic(element: HTMLElement) {
     if (element.className === "nav") {
         element.className += " responsive";
     }
     else {
         element.className = "nav";
     }
+}
+function compactNav() {
+    const element = document.getElementById("topNav") as HTMLElement;
+    compactNavLogic(element);
 }
 
 function compactNav2() {
     const element = document.getElementById("topNav2") as HTMLElement;
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
+    compactNavLogic(element);
 }
 
 function compactNav3() {
     const element = document.getElementById("topNav3") as HTMLElement;
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
+    compactNavLogic(element);
 }
 
 function compactNav4() {
     const element = document.getElementById("topNav4") as HTMLElement;
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
+    compactNavLogic(element);
 }
 
 function compactNav5() {
     const element = document.getElementById("topNav5") as HTMLElement;
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
+    compactNavLogic(element);
 }
 
 function compactNav6() {
     const element = document.getElementById("topNav6") as HTMLElement;
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
+    compactNavLogic(element);
 }
 
 function compactNav7() {
     const element = document.getElementById("topNav7") as HTMLElement;
-    if (element.className === "nav") {
-        element.className += " responsive";
-    }
-    else {
-        element.className = "nav";
-    }
+    compactNavLogic(element);
 }
 
 /* eslint-enable @typescript-eslint/no-unused-vars
